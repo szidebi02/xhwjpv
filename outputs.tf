0350a509556d16f54bd63d77d50fa02d34e5fd23
Debi <103049657+szidebi02@users.noreply.github.com>
2023-05-24 20:41:33 +0200

    task3 javitva

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "read_output"{
+    value = module.read_output
+}
+
+output "answer_1"{
+    value = module.write.answer_1
+}
+output "answer_2"{
+    value = module.write.answer_2
+}
+output "answer_3"{
+    value = module.write.answer_3
+}
+output "answer_4"{
+    value = module.write.answer_4
+}
+output "answer_5"{
+    value = module.write.answer_5
+}
